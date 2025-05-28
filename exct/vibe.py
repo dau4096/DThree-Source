954a2c09449f9d8e24eaa468b72dc5cce77453d6
TheMightyHowitzer <83647038+TheMightyHowitzer@users.noreply.github.com>
2025-05-28 19:01:31 +0200

    Fixed using wrong syntax
    Removed "then" from if statement.

--- exct/vibe.py
+++ exct/vibe.py
@@ -56,7 +56,7 @@ class Song:
 
 	def __repr__(self) -> str:
 		#When printing to console.
-		if self.duration > 60 then:
+		if self.duration > 60:
 			return f"<Song: [{self.name} by {self.artist}, suggested by {self.suggestedBy} | duration: Appelpie | genres: {self.genres} | intensity: {self.intensities} | mood: {self.moods} | url: {self.url}]>"
 		else:
 			return f"<Song: [{self.name} by {self.artist}, suggested by {self.suggestedBy} | duration: {self.duration}s | genres: {self.genres} | intensity: {self.intensities} | mood: {self.moods} | url: {self.url}]>"
