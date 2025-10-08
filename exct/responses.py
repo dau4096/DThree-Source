f15943cd420ee1d17a07e8cf45d786e1c0b3c171
Dau4096 <mrdau4096@gmail.com>
2025-10-08 12:01:13 +0100

    Testing manager script

--- exct/responses.py
+++ exct/responses.py
@@ -559,7 +559,7 @@ async def checkReplies(messageData: str, message: discord.Message) -> None:
 		return
 		
 	elif ("budget" in messageData):
-		await sendMessage(message, "fr")
+		await sendMessage(message, "fr fr ong no cap")
 		return
 		
 	elif any((phrase in messageData for phrase in ("denmark", "dane", "danmark", "dansk"))):
