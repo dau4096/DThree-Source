da4064a5050f3e07b687fb6e232b313026bcb13a
Dau4096 <mrdau4096@gmail.com>
2025-11-29 16:34:41 +0000

    Intersect not Union

--- exct/artbook.py
+++ exct/artbook.py
@@ -109,8 +109,7 @@ def filePathSortFunction(a) -> bool:
 def searchArtbookIndexFileByList(tags:list[str]) -> list[str]:
 	total:set[str] = set(searchArtbookIndexFile(tags[0]));
 	if (len(tags) > 1):
-		for tag in tags[1:]: total = total.union(set(searchArtbookIndexFile(tag)));
-	print(tags)
+		for tag in tags[1:]: total = total.intersect(set(searchArtbookIndexFile(tag)));
 	return list(total);
 
 
