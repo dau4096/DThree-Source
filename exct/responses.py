581971e79273598eae2692cda250f5966a684d16
Dau4096 <mrdau4096@gmail.com>
2025-11-29 16:25:08 +0000

    sorted

--- exct/responses.py
+++ exct/responses.py
@@ -535,7 +535,7 @@ async def checkReplies(messageData: str, message: discord.Message) -> None:
 
 	if (messageData.startswith("/artbook-tags")):
 		tags:set[str] = getArtbookTags();
-		await replyMessage(message, f"All usable tags: {[t.title() for t in tags].sort()}");
+		await replyMessage(message, f"All usable tags: {sorted([t.title() for t in tags])}");
 
 
 	if messageData.startswith("/serverage"):
