cc2d91e001fcb1a25d2ef5585a45067e5e3680a9
Dau4096 <mrdau4096@gmail.com>
2025-11-29 14:59:31 +0000

    Post images

--- exct/artbook.py
+++ exct/artbook.py
@@ -7,7 +7,6 @@ import random, os;
 
 global BOOKS, TAGS;
 ARTBOOK_DIR:str = f"{os.getenv('TXT_DIR')}/artbook";
-print(ARTBOOK_DIR);
 
 
 
@@ -97,7 +96,7 @@ def parseIndexFile() -> None:
 def filePathSortFunction(a) -> bool:
 	try:
 		return int(regex.sub(
-			r"(?i)^.*/|\..*\.png$", "",
+			r"(?i)^.*/|\..*\.jpg$", "",
 			a
 		)); #Return just the page number.
 	except ValueError: #Failed to convert to int; filepath may be invalid.
